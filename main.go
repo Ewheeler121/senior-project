10739348cc784848cec50423b1505e8caafe5217
Derekdrc <derekdrc@yahoo.com>
2025-04-02 00:28:00 -0400

    Updated styling and look for multiple pages

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 	"sync"
 
-    "github.com/gorilla/context"
+	"github.com/gorilla/context"
 	"github.com/gorilla/sessions"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -25,86 +25,85 @@ var mu sync.Mutex
 var cookies *sessions.CookieStore
 
 func main() {
-    debugPrint("SESSION_KEY: ", os.Getenv("SESSION_KEY"))
-    debugPrint("API_KEY: ", os.Getenv("API_KEY"))
+	debugPrint("SESSION_KEY: ", os.Getenv("SESSION_KEY"))
+	debugPrint("API_KEY: ", os.Getenv("API_KEY"))
 
-    if os.Getenv("SESSION_KEY") == "" {
-        //TODO: remove the set after testing
-        //panic("environment variable SESSION_KEY is not set, run\nexport SESSION_KEY={session key}")
-        os.Setenv("SESSION_KEY", "debugging")
-    }
-	
-    if os.Getenv("API_KEY") == "" {
-        panic("environment variable API_KEY is not set, run\nexport API_KEY={api key}")
-    }
+	if os.Getenv("SESSION_KEY") == "" {
+		//TODO: remove the set after testing
+		//panic("environment variable SESSION_KEY is not set, run\nexport SESSION_KEY={session key}")
+		os.Setenv("SESSION_KEY", "debugging")
+	}
 
-    init_database()
-    defer db.Close()
+	if os.Getenv("API_KEY") == "" {
+		panic("environment variable API_KEY is not set, run\nexport API_KEY={api key}")
+	}
 
-    tpl = template.Must(template.ParseGlob("templates/*.html"))
-    cookies = sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
-    
+	init_database()
+	defer db.Close()
 
-    http.HandleFunc("/loginauth", loginAuthHandler)
-    http.HandleFunc("/login", loginPageHandler)
-    http.HandleFunc("/logout", auth(logoutAuthHandler))
-    http.HandleFunc("/registerauth", registerAuthHandler)
-    http.HandleFunc("/register", registerPageHandler)
-    
-    http.HandleFunc("/submit", auth(submitPageHandler))
-    http.HandleFunc("/submitPost", auth(submitPostHandler))
-    http.HandleFunc("/download/", posterDownloadHandler)
-    http.HandleFunc("/poster/", posterPageHandler)
-    http.HandleFunc("/edit/", auth(editEntryHandler))
-    http.HandleFunc("/delete/", deleteEntryHandler)
-    http.HandleFunc("/editEntry/", auth(editEntryPostHandler))
-    http.HandleFunc("/addFile/", auth(addFileHandler))
-    http.HandleFunc("/replaceFile/", auth(replaceFileHandler))
-    http.HandleFunc("/deleteFile/", auth(deleteFileHandler))
-    
-    http.HandleFunc("/searchPost", searchPostHandler)
-    http.HandleFunc("/search", searchPageHandler)
-    
-    http.HandleFunc("/about", aboutHandler)
-    http.HandleFunc("/favicon.ico", faviconHandler)
-    http.HandleFunc("/", nullHandler)
+	tpl = template.Must(template.ParseGlob("templates/*.html"))
+	cookies = sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
 
-    //TODO: remove this line after testing
-    err := http.ListenAndServeTLS(":443", "domain.cert.pem", "private.key.pem", context.ClearHandler(http.DefaultServeMux))
-    //err := http.ListenAndServe("localhost:8080", context.ClearHandler(http.DefaultServeMux))
-    if err != nil {
-        debugPrint("cannot start server ", err)
-        fmt.Fprintln(os.Stderr, "ERROR: could not start server, ", err)
-    }
+	http.HandleFunc("/loginauth", loginAuthHandler)
+	http.HandleFunc("/login", loginPageHandler)
+	http.HandleFunc("/logout", auth(logoutAuthHandler))
+	http.HandleFunc("/registerauth", registerAuthHandler)
+	http.HandleFunc("/register", registerPageHandler)
+
+	http.HandleFunc("/submit", auth(submitPageHandler))
+	http.HandleFunc("/submitPost", auth(submitPostHandler))
+	http.HandleFunc("/download/", posterDownloadHandler)
+	http.HandleFunc("/poster/", posterPageHandler)
+	http.HandleFunc("/edit/", auth(editEntryHandler))
+	http.HandleFunc("/delete/", deleteEntryHandler)
+	http.HandleFunc("/editEntry/", auth(editEntryPostHandler))
+	http.HandleFunc("/addFile/", auth(addFileHandler))
+	http.HandleFunc("/replaceFile/", auth(replaceFileHandler))
+	http.HandleFunc("/deleteFile/", auth(deleteFileHandler))
+
+	http.HandleFunc("/searchPost", searchPostHandler)
+	http.HandleFunc("/search", searchPageHandler)
+
+	http.HandleFunc("/about", aboutHandler)
+	http.HandleFunc("/favicon.ico", faviconHandler)
+	http.HandleFunc("/", nullHandler)
+
+	//TODO: remove this line after testing
+	//err := http.ListenAndServeTLS(":443", "domain.cert.pem", "private.key.pem", context.ClearHandler(http.DefaultServeMux))
+	err := http.ListenAndServe("localhost:8080", context.ClearHandler(http.DefaultServeMux))
+	if err != nil {
+		debugPrint("cannot start server ", err)
+		fmt.Fprintln(os.Stderr, "ERROR: could not start server, ", err)
+	}
 }
 
 func nullHandler(w http.ResponseWriter, r *http.Request) {
-    if r.URL.Path[len("/"):] == "" {
-        indexHandler(w, r)
-        return
-    }
-    if strings.HasSuffix(r.URL.Path, ".css") {
-        http.ServeFile(w, r, "static/css" + r.URL.Path)
-        return
+	if r.URL.Path[len("/"):] == "" {
+		indexHandler(w, r)
+		return
 	}
-    if strings.HasSuffix(r.URL.Path, ".webp") || strings.HasSuffix(r.URL.Path, ".ico") {
-        http.ServeFile(w, r, "static/img" + r.URL.Path)
-        return
+	if strings.HasSuffix(r.URL.Path, ".css") {
+		http.ServeFile(w, r, "static/css"+r.URL.Path)
+		return
+	}
+	if strings.HasSuffix(r.URL.Path, ".webp") || strings.HasSuffix(r.URL.Path, ".ico") {
+		http.ServeFile(w, r, "static/img"+r.URL.Path)
+		return
 	}
 	http.NotFound(w, r)
 }
 
 func init_database() {
-    var err error
-    db, err = sql.Open("sqlite3", "./database.db")
-    if err != nil {
-        panic(err.Error())
-    }
-    
-    //TODO: add more to users for profile page
-    //TODO: modify Entries table to include more categories etc..
-    //TODO: modify Files table to include more categories
-    _, err = db.Exec(`
+	var err error
+	db, err = sql.Open("sqlite3", "./database.db")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	//TODO: add more to users for profile page
+	//TODO: modify Entries table to include more categories etc..
+	//TODO: modify Files table to include more categories
+	_, err = db.Exec(`
         CREATE TABLE IF NOT EXISTS users (
             username TEXT NOT NULL UNIQUE,
             password TEXT NOT NULL,
@@ -153,41 +152,40 @@ func init_database() {
         );
     `)
 
-    if err != nil {
-        panic(err.Error())
-    }
+	if err != nil {
+		panic(err.Error())
+	}
 }
 
 func renderTemplate(w http.ResponseWriter, r *http.Request, tplFile string, title string, data tplData) {
-    if data == nil {
-        data = tplData {
-            "Title": title,
-        }
-    } else {
-        data["Title"] = title
-    }
+	if data == nil {
+		data = tplData{
+			"Title": title,
+		}
+	} else {
+		data["Title"] = title
+	}
 
-    user := getUser(r)
-    if user != "" {
-        data["User"] = user
-    }
+	user := getUser(r)
+	if user != "" {
+		data["User"] = user
+	}
 
-    err := tpl.ExecuteTemplate(w, tplFile, data)
-    if err != nil {
-        debugPrint("Error Rendering Template", err)
-        http.Error(w, "Error Rendering Template", http.StatusInternalServerError)
-    }
+	err := tpl.ExecuteTemplate(w, tplFile, data)
+	if err != nil {
+		debugPrint("Error Rendering Template", err)
+		http.Error(w, "Error Rendering Template", http.StatusInternalServerError)
+	}
 }
 
 func faviconHandler(w http.ResponseWriter, r *http.Request) {
-    http.ServeFile(w, r, "static/img/favicon.ico")
+	http.ServeFile(w, r, "static/img/favicon.ico")
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-    renderTemplate(w, r, "index.html", "Home", nil)
+	renderTemplate(w, r, "index.html", "Home", nil)
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
-    renderTemplate(w, r, "about.html", "About", nil)
+	renderTemplate(w, r, "about.html", "About", nil)
 }
-
